8f5703aae511c39f411971b8cba493df21d3dd4d
Samuel Adekunle <samuelbeebest@gmail.com>
2021-06-23 10:43:42 +0100

    updates

--- lib/login_ui.dart
+++ lib/login_ui.dart
@@ -6,6 +6,7 @@ class LoginUI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Color color = Color(0xFF5D5167).withOpacity(0.5);
     return Scaffold(
       body: Container(
         color: Colors.red,
@@ -13,18 +14,19 @@ class LoginUI extends StatelessWidget {
           child: Card(
             elevation: 1,
             color: Color(0xFF251734).withOpacity(0.5),
+            margin: EdgeInsets.all(16),
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(22)),
             child: Container(
-              height: 220,
+              height: 300,
               width: MediaQuery.of(context).size.width,
               child: Column(
                 children: [
                   Container(
-                    // #5D5167
                     width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.only(top: 18, bottom: 18),
                     decoration: BoxDecoration(
-                      color: Color(0xFF5D5167).withOpacity(0.5),
+                      color: color,
                       borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(22),
                         topRight: Radius.circular(22),
@@ -33,7 +35,12 @@ class LoginUI extends StatelessWidget {
                     child: Center(
                       child: Text(
                         'CUSTOMER LOGIN',
-                        style: GoogleFonts.sourceSansPro(color: Colors.white),
+                        style: GoogleFonts.raleway(
+                          color: Colors.white,
+                          letterSpacing: 1.5,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
                       ),
                     ),
                   ),
@@ -55,23 +62,44 @@ class LoginUI extends StatelessWidget {
                             prefixIcon: Icon(Icons.lock),
                           ),
                         ),
+                        SizedBox(height: 12),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text('Remember me'),
-                            Text('Forgot Password?')
+                            Text(
+                              'Remember me',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w100,
+                                color: Color(0xFF5D5167),
+                              ),
+                            ),
+                            Text(
+                              'Forgot Password?',
+                              style: TextStyle(
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.w100,
+                                color: Color(0xFF5D5167),
+                              ),
+                            )
                           ],
                         ),
+                        SizedBox(height: 12),
                         MaterialButton(
                           onPressed: () {},
-                          child: Text('LOGIN'),
+                          child: Text(
+                            'LOGIN',
+                            style: TextStyle(
+                              letterSpacing: 1.2,
+                              fontWeight: FontWeight.w100,
+                            ),
+                          ),
                           elevation: 0,
                           textColor: Colors.white,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(8)),
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 50, vertical: 11),
-                          color: Color(0xFF5D5167).withOpacity(0.5),
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 60, vertical: 8),
+                          color: color,
                         )
                       ],
                     ),
